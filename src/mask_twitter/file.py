ea70a31755a118b38910b31a808cf43907fbe587
Mask-coins <uton_yayuyo_ics@yahoo.co.jp>
2022-04-07 12:14:26 +0900

    FileWriter

---
+++ src/mask_twitter/file.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+import json
+
+class FileWriter(object):
+    def __init__(self, file_path):
+        self._file_path = file_path
+
+    def add(self, obj:dict|list):
+        with open(self._file_path, encoding="utf-8", mode='a') as fp:
+            fp.write(json.dumps(obj=obj, ensure_ascii=False))
+
+
--- src/mask_twitter/tools.py
+++ src/mask_twitter/tools.py
@@ -1,24 +1,8 @@
 from __future__ import annotations
 
-import json
-
 import tweepy
 from tweepy.models import User as TweepyUser
 from tweepy.errors import TweepyException
-
-
-class FileWriter(object):
-    def __init__(self, file_path):
-        self._file_path = file_path
-
-    def add(self, obj:dict|list):
-        with open(self._file_path, encoding="utf-8", mode='a') as fp:
-            fp.write(json.dumps(obj=obj, ensure_ascii=False))
-
-
-
-
-
 
 
 class TweetGetter(object):
