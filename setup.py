d68d58c265cee56e7e2d6dd5874128b7c74d5b19
Mask-coins <uton_yayuyo_ics@yahoo.co.jp>
2022-04-09 17:52:08 +0900

    TweetCollectorSystem

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def _requires_from_file(filename):
 
 setup(
     name="mask_twitter",
-    version="0.0.3",
+    version="0.0.4",
     description="",
     author="Mask_coins",
     url="https://github.com/Mask-coins/mask_twitter",
