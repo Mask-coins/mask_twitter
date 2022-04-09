964c3d564353b1eec060cb3dc3a84c586c9f452c
Mask-coins <uton_yayuyo_ics@yahoo.co.jp>
2022-04-09 19:42:27 +0900

    TweetGetter

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def _requires_from_file(filename):
 
 setup(
     name="mask_twitter",
-    version="0.0.13",
+    version="0.0.14",
     description="",
     author="Mask_coins",
     url="https://github.com/Mask-coins/mask_twitter",
